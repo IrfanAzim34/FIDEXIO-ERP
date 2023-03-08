58a2bb5d046657d3e56464a7822789c5f445de68
Irfan Azim <irfanteipov@gmail.com>
2023-03-08 04:17:08 +0300

    Added some Utilities classes and Runner Classes

---
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.fidexio.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber_reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "src/test/java/com/fidexio/step_defs",
+        dryRun = true,
+        publish = true,
+        tags = ""
+)
+public class CukesRunner {
+
+}
